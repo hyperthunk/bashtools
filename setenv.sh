d126ba14d4a0fee6823fdd5f28fe7242555c46f6
Tim Watson <watson.timothy@gmail.com>
2010-08-25 10:31:10 +0100

    removing rebar alias

--- setenv.sh
+++ setenv.sh
@@ -2,5 +2,3 @@ export ERL_ROOT=$ERL_TOP/Current
 export ERL_LIBS=$ERL_TOP/Site
 export MANPATH=$ERL_ROOT/man:$MANPATH
 export PATH=$ERL_ROOT/bin:$PATH
-alias rebar='env ERL_LIBS="./deps:$ERL_LIBS" $HOME/bin/rebar.escript skip_deps=true $1'
-
